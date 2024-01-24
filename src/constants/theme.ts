ee95360a634d6e91905f300e7795a0982112c7aa
Harshit Duggal <harshitduggal101@gmail.com>
2024-01-24 13:48:41 +0530

    chore: initialize project setup

---
+++ src/constants/theme.ts
@@ -0,0 +1,20 @@
+//colors used
+export const Colors = {
+  lightYellow: '#FFC83A',
+  darkYellow: '#F9B023',
+  lightBlue: '#2A4BA0',
+  darkBlue: '#153075',
+  gray1: '#F8F9FB',
+  gray2: '#E7ECF0',
+  gray3: '#C5CDD2',
+  gray4: '#A9B4BC',
+  gray5: '#606D76',
+  gray6: '#354349',
+  black: '#1B262E',
+};
+
+//sizes used
+export const Sizes = {
+  iconSize: 25,
+  fonstSize: 20,
+};
